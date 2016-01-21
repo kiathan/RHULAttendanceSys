29724fb526e537fda6a98ffe1f74655ca11b59cb
JihaKim <zavb039@live.rhul.ac.uk>
2016-01-22 05:30:08 +0900

    PHP tutorials
    Variables, switch, echo...

---
+++ workshop_jiha/test1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>PHP FTW!</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <?php
+        $myName = "Jiha";
+        $myAge = 23;
+        echo $myName;
+        echo $myAge;
+        ?>
+        <p>
+            
+        </p>   
+	</body>
+</html>
---
+++ workshop_jiha/test2.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Write your if/elseif/else statement here!
+        if(2>3){
+            echo "The condition is true";}
+        else{
+            echo "The condition is false";
+            }
+      ?>
+    </p>
+  </body>
+</html>

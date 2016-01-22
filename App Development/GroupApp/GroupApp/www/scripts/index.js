fe1721d456626d608e1607cc078d29f582e5c20c
kiathan <kiat.kok.2014@live.rhul.ac.uk>
2016-01-22 16:02:53 +0000

    Login Page Update 1
    Loaded native notification.
    Login button works once and stopped working.

--- App Development/GroupApp/GroupApp/www/scripts/index.js
+++ App Development/GroupApp/GroupApp/www/scripts/index.js
@@ -1,27 +1,43 @@
-﻿// For an introduction to the Blank template, see the following documentation:
+﻿ // For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints,
 // and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+(function() {
+  "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+  document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+  $('.login-btn').click(login);
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        $('#logInBT').click(function() {
-            //
-        });
-    };
+  function onDeviceReady() {
+    // Handle the Cordova pause and resume events
+    document.addEventListener('pause', onPause.bind(this), false);
+    document.addEventListener('resume', onResume.bind(this), false);
+    //native popup
+    if (navigator.notification) { // Override default HTML alert with native dialog
+      window.alert = function(message) {
+        navigator.notification.alert(
+          message, // message
+          null, // callback
+          "Royal Ray", // title
+          'OK' // buttonName
+        );
+      };
+    }
+  };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+  function login() {
+    var u = $("#username").val();
+    var p = $("#password").val();
+    alert("hello," + u + " " + p);
+  };
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+  function onPause() {
+    // TODO: This application has been suspended. Save application state here.
+  };
+
+  function onResume() {
+    // TODO: This application has been reactivated. Restore application state here.
+  };
+
+
+})();

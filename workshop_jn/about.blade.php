26d378ec2a29fee06ff0c48c036d6b2fe6ce17d1
Jnunoi <jnuno.carrilho@gmail.com>
2016-01-22 10:07:31 +0000

    Code generated after by following Bartals tutorial on laravel

---
+++ workshop_jn/about.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.master')
+@section('content')
+<div class="col-xs-8">
+	<div class="row">
+		This is a page about us
+	</div>
+	<div class="row">
+		Person one
+	</div>
+	<div class="row">
+		Person one
+	</div>
+	<div class="row">
+		Person one
+	</div>
+</div>
+<div class="col-xs-3 col-xs-offset-1">
+	<div class="row">
+		We are open from 8.00 am to 7.00 pm every day of the week
+	</div>
+</div>
+@stop
---
+++ workshop_jn/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.master')
+@section('content')
+<div class="col-xs-12">
+	<div class="row">
+		<div class="well">
+			Hello and welcome to my page :D
+		</div>
+	</div>
+</div>
+@stop

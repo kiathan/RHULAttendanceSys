e79a92784f19f56eaa18b1c53e2961c8b7bf758f
Shaofeng-Liu <zbac009@rhul.ac.uk>
2016-01-22 09:03:18 +0000

    Cordova tutorial exercise
    I have Finished tutorial step 11 and it worked on android but not on
    iOS.

---
+++ workshop_S_Liu/CDAtest/MyAndroid/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.0",
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-dialogs": "1.2.0"
+}
+// BOTTOM OF METADATA
+});

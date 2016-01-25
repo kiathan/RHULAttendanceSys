9bbbc450fb680ffb44139b66ce9f02161782ca2e
dancoles <zavc386@live.rhul.ac.uk>
2016-01-25 13:13:27 +0000

    Added a timetable page
    Incomplete

--- App Development/GroupApp/GroupApp/platforms/android/assets/www/scripts/index.js
+++ App Development/GroupApp/GroupApp/platforms/android/assets/www/scripts/index.js
@@ -32,9 +32,14 @@
    * Handles logging in of the account and saving the session on the device.
    **/
   function login() {
+    //retrieves username and password from the fields.
     var u = $("#username").val();
     var p = $("#password").val();
     alert("hello," + u + " " + p);
+    //TODO: input validation
+    //TODO: calls webserver to attempt to login
+    //TODO: checks login status and decides if user should login.
+    //If login is successful, directs to landing page.
     window.location.href = "#Landing";
   };
 
@@ -43,6 +48,7 @@
    **/
   function logout() {
     alert("bye");
+    //TODO: Clears session related data
     window.location.href = "#logIn";
   }
 

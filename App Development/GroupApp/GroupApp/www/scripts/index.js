0fdc98231f356c9ee5b79ed56ccfc4858b5d5168
Shaofeng-Liu <zbac009@rhul.ac.uk>
2016-01-26 18:04:37 +0000

    Made buttons to radio button
    changed the student submit button to radio button, but needs to change
    it back

--- App Development/GroupApp/GroupApp/www/scripts/index.js
+++ App Development/GroupApp/GroupApp/www/scripts/index.js
@@ -1,81 +1,88 @@
-﻿ // For an introduction to the Blank template, see the following documentation:
+﻿// For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints,
 // and then run "window.location.reload()" in the JavaScript Console.
-(function() {
-  "use strict";
+(function () {
+    "use strict";
 
-  document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
-  function onDeviceReady() {
-    // Handle the Cordova pause and resume events
-    document.addEventListener('pause', onPause.bind(this), false);
-    document.addEventListener('resume', onResume.bind(this), false);
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
 
-    //calls the login function when login button is clicked.
+        //calls the login function when login button is clicked.
 
-    $('.login-btn').click(login);
-    $('.logout-btn').click(logout);
+        $('.login-btn').click(login);
+        $('.logout-btn').click(logout);
 
-    //call server to increase answer count
-      $("button").click(function() {
-          alert(this.id); // or alert($(this).attr('id'));
-      });
-    //native popup
-    if (navigator.notification) { // Override default HTML alert with native dialog
-      window.alert = function(message) {
-        navigator.notification.alert(
-          message, // message
-          null, // callback
-          "Royal Ray", // title
-          'OK' // buttonName
-        );
-      };
+        //call server to increase answer count
+        $('.answer-btn').click(function () {
+            var radios = document.getElementsByName("rdoBtn");
+            for (var i=0;i<radios.length;i++){
+                if (radios[i].checked){
+                    alert(radios[i].value);
+                    break;
+                }
+            }
+        });
+
+        //native popup
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message, // message
+                    null, // callback
+                    "Royal Ray", // title
+                    'OK' // buttonName
+                );
+            };
+        }
+    };
+
+    /**
+     * Handles logging in of the account and saving the session on the device.
+     **/
+
+
+    function login() {
+        //retrieves username and password from the fields.
+        var u = $("#username").val();
+        var p = $("#password").val();
+        alert("hello," + u + " " + p);
+        //TODO: input validation
+        //TODO: calls webserver to attempt to login
+        //TODO: checks login status and decides if user should login.
+        //If login is successful, directs to landing page.
+        var isStudent = true;
+        if (u.localeCompare("lecturer") == 0) {
+            isStudent = false;
+        }
+        if (isStudent) {
+            window.location.href = "#StudentLanding";
+        } else {
+            window.location.href = "#LecturerLanding";
+        }
+
+    };
+
+    /**
+     * Handles logging out of the account and clearing storage
+     **/
+    function logout() {
+        alert("bye");
+        //TODO: Clears session related data
+        window.location.href = "#logIn";
     }
-  };
 
-  /**
-   * Handles logging in of the account and saving the session on the device.
-   **/
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
 
-
-  function login() {
-    //retrieves username and password from the fields.
-    var u = $("#username").val();
-    var p = $("#password").val();
-    alert("hello," + u + " " + p);
-    //TODO: input validation
-    //TODO: calls webserver to attempt to login
-    //TODO: checks login status and decides if user should login.
-    //If login is successful, directs to landing page.
-    var isStudent = true;
-    if (u.localeCompare("lecturer") == 0) {
-      isStudent = false;
-    }
-    if (isStudent) {
-      window.location.href = "#StudentLanding";
-    } else {
-      window.location.href = "#LecturerLanding";
-    }
-
-  };
-
-  /**
-   * Handles logging out of the account and clearing storage
-   **/
-  function logout() {
-    alert("bye");
-    //TODO: Clears session related data
-    window.location.href = "#logIn";
-  }
-
-  function onPause() {
-    // TODO: This application has been suspended. Save application state here.
-  };
-
-  function onResume() {
-    // TODO: This application has been reactivated. Restore application state here.
-  };
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
 
 
 })();

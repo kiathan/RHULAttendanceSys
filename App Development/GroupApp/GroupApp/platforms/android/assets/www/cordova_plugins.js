d3bc8ae90fa4524e6386ce47248a8160a95c76ce
dancoles <zavc386@live.rhul.ac.uk>
2016-01-28 12:57:19 +0000

    Timetable page added
    responsive table is working fine in android emulator but not on my
    android device??

--- App Development/GroupApp/GroupApp/platforms/android/assets/www/cordova_plugins.js
+++ App Development/GroupApp/GroupApp/platforms/android/assets/www/cordova_plugins.js
@@ -3,13 +3,11 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     },
     {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
         "clobbers": [
             "device"
         ]
@@ -17,7 +15,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "id": "cordova-plugin-dialogs.notification",
-        "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
         ]
@@ -25,7 +22,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
         "id": "cordova-plugin-dialogs.notification_android",
-        "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
         ]
@@ -35,8 +31,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-device": "1.1.1",
     "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-device": "1.1.1",
     "cordova-plugin-dialogs": "1.2.0"
 }
 // BOTTOM OF METADATA
--- App Development/GroupApp/GroupApp/platforms/ios/www/cordova_plugins.js
+++ App Development/GroupApp/GroupApp/platforms/ios/www/cordova_plugins.js
@@ -1,17 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
         "id": "cordova-plugin-console.console",
-        "pluginId": "cordova-plugin-console",
         "clobbers": [
             "console"
         ]
@@ -19,15 +10,20 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-console/www/logger.js",
         "id": "cordova-plugin-console.logger",
-        "pluginId": "cordova-plugin-console",
         "clobbers": [
             "cordova.logger"
         ]
     },
     {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "id": "cordova-plugin-dialogs.notification",
-        "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
         ]
@@ -37,8 +33,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-device": "1.1.1",
     "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-device": "1.1.1",
     "cordova-plugin-dialogs": "1.2.0"
 }
 // BOTTOM OF METADATA

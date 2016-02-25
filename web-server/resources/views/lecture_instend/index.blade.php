b5d8d56b8a543c3e317d1e53f83a6e0759a5d840
bartal veyhe <bartal35@gmail.com>
2016-02-25 17:18:36 +0000

    Add start time and end time to the index list of active lecture instancets

--- web-server/resources/views/lecture_instend/index.blade.php
+++ web-server/resources/views/lecture_instend/index.blade.php
@@ -28,7 +28,7 @@
                 @foreach($lecture_instends as $lecture)
                     <tr>
                         <td><a href="{{url("/lecture_instends/show/" . $lecture->id)}}">{{$lecture->id}} </a></td>
-                        <td> {{$lecture->lecture->course->name}} - {{$lecture->lecture->dayofweek}} </td>
+                        <td> {{$lecture->lecture->course->name}} - {{$lecture->lecture->dayofweek}} <br> {{$lecture->lecture->starttime}} - {{$lecture->lecture->endtime}} </td>
                         <td> {{$lecture->isActive}}</td>
                         <td>{{$lecture->created_at}}</td>
                         <td>{{$lecture->updated_at}}</td>

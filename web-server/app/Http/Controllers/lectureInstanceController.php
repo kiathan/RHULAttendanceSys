ece877a42b69d0e7179f1acb25686d72a74d366a
bartal veyhe <bartal35@gmail.com>
2016-02-25 17:16:34 +0000

    Update the lecture controller to use size of array instend of null to check if there is any lectrue that can curreny be userd

--- web-server/app/Http/Controllers/lectureInstanceController.php
+++ web-server/app/Http/Controllers/lectureInstanceController.php
@@ -26,7 +26,7 @@ class lectureInstanceController extends Controller
             $lecutesInstance = $user->getCurrentLectureInstance();
 
             $checkOfActiveCount = count($lecutesInstance, 1);
-            if($checkOfActiveCount == 0){
+            if ($checkOfActiveCount == 0) {
                 $response['state'] = 'failure';
                 $response['message'] = 'No lecture is active right now';
 
@@ -36,7 +36,8 @@ class lectureInstanceController extends Controller
             return json_encode($user->getCurrentLectureInstance());
         }
 
-        $lecture_instends = \App\lecture_instend::where('isActive', '1')->get();
+        $lecture_instends = \App\lecture_instend::where('isActive', '1')->get()->load('lecture');
+
         return view('lecture_instend/index')->with(['lecture_instends' => $lecture_instends]);
     }
 
@@ -149,7 +150,7 @@ class lectureInstanceController extends Controller
             $lecture_instances = $lecture_instances[0];
             $instanceAuth = NULL;
             foreach ($lecture_instances as $lecture_instance) {
-                if($lecture_instance->checkQRcode($request->get('lectureAuthCode'))) {
+                if ($lecture_instance->checkQRcode($request->get('lectureAuthCode'))) {
                     $instanceAuth = $lecture_instance;
                     break;
                 }
@@ -193,15 +194,21 @@ class lectureInstanceController extends Controller
     public function createLectureInstance(Request $request)
     {
         $user = \App\User::find($request->get('user_id'));
-        if (!$user->currentLectures()) {
+        if (sizeof($user->currentLectures()) == 0) {
             \App\lecture::createRandomLecture($user);
         }
 
-        $lecutre = $user->currentLectures();
-        if (\App\lecture_instend::where('isActive', 'true')->where('lecture_id', $lecutre->id)->first()) {
-            return redirect("/");
+        $lecutres = $user->currentLectures();
+        foreach ($lecutres as $lecutre) {
+            if (\App\lecture_instend::where('isActive', 'true')->where('lecture_id', $lecutre->id)->first()) {
+                return redirect("/");
+            }
         }
-        \App\lecture_instend::create(['lecture_id' => $lecutre->id, 'isActive' => '1']);
+
+        foreach ($lecutres as $lecutre) {
+            \App\lecture_instend::create(['lecture_id' => $lecutre->id, 'isActive' => '1']);
+            break;
+        }
         return redirect("/");
     }
 

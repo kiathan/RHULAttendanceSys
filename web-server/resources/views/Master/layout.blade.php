3b2c14e1f6dbf2c93f164557c3a2b419c61bbf95
JihaKim <zavb039@live.rhul.ac.uk>
2016-03-09 00:06:56 +0900

    link problem
    solved the link (moodle) problem

--- web-server/resources/views/Master/layout.blade.php
+++ web-server/resources/views/Master/layout.blade.php
@@ -129,7 +129,7 @@
 		<div class = "container">
 		
 			<p class = "navbar-text pull-left">Site Built By Team6</p>
-			<a href = "moodle.rhul.ac.uk" class = "navbar-btn btn-danger btn pull-right" data-toggle="tooltip" title="Link to Moodle">RHUL MOODLE</a>
+			<a href = "http://moodle.rhul.ac.uk" class = "navbar-btn btn-danger btn pull-right" data-toggle="tooltip" title="Link to Moodle">RHUL MOODLE</a>
 		
 		</div>
 	</div>

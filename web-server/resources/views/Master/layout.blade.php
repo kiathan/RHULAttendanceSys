f799c4e422f35936d080904cea4d9100b7496bdb
JihaKim <zavb039@live.rhul.ac.uk>
2016-03-09 07:12:22 +0900

    edited typo

--- web-server/resources/views/Master/layout.blade.php
+++ web-server/resources/views/Master/layout.blade.php
@@ -44,7 +44,7 @@
 							<label for="password" class="col-sm-2 control-label">Password</label>
 							<div class="col-sm-10">
 
-								<input type="password" id="password" name="password" class="form-control" placeholder="passw0rd">
+								<input type="password" id="password" name="password" class="form-control" placeholder="password">
 
 							</div>
 						</div>

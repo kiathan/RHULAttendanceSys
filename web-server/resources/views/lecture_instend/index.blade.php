b3b2c8604eebdb944426310918b428c355713fea
bartal veyhe <bartal35@gmail.com>
2016-03-11 10:23:56 +0000

    remvoe import of javascript libarys

--- web-server/resources/views/lecture_instend/index.blade.php
+++ web-server/resources/views/lecture_instend/index.blade.php
@@ -1,43 +1,45 @@
-@extends('Master.layout')
+<html>
+<head></head>
+<body>
+<style>
+    table {
+        width: 100%;
+    }
 
+    table, td, tr, th {
+        border: 1px solid black;
+        text-align: center;
+    }
 
-@section('content')
-    <style>
-        table {
-            width: 100%;
-        }
-        table, td, tr, th{
-            border:1px solid black;
-            text-align: center;
-        }
+</style>
+<div>
+    @if(sizeof($lecture_instends) == 0)7
+    <p>There are currenly no active instances</p>
+    @else
+        <table>
 
-    </style>
-    <div>
-        @if(sizeof($lecture_instends) == 0)7
-        <p>There are currenly no active instances</p>
-        @else
-            <table>
-
-                <tr>
-                    @foreach(array_keys($lecture_instends[0]->toArray()) as $names)
-                        <th>{{$names}}</th>
-                    @endforeach
-                    <th>Qr code for auth</th>
-                    <th>Qr code result</th>
-                </tr>
-                @foreach($lecture_instends as $lecture)
-                    <tr>
-                        <td><a href="{{url("/lecture_instends/show/" . $lecture->id)}}">{{$lecture->id}} </a></td>
-                        <td> {{$lecture->lecture->course->name}} - {{$lecture->lecture->dayofweek}} <br> {{$lecture->lecture->starttime}} - {{$lecture->lecture->endtime}} </td>
-                        <td> {{$lecture->isActive}}</td>
-                        <td>{{$lecture->created_at}}</td>
-                        <td>{{$lecture->updated_at}}</td>
-                        <td>{!! $lecture->sendQRcode() !!}</td>
-                        <td>{!! $lecture->createHash() !!}</td>
-                        <td></td>
-                    </tr>
+            <tr>
+                @foreach(array_keys($lecture_instends[0]->toArray()) as $names)
+                    <th>{{$names}}</th>
                 @endforeach
-            </table>
-        @endif
-    </div>
-@stop
+                <th>Qr code for auth</th>
+                <th>Qr code result</th>
+            </tr>
+            @foreach($lecture_instends as $lecture)
+                <tr>
+                    <td><a href="{{url("/lecture_instends/show/" . $lecture->id)}}">{{$lecture->id}} </a></td>
+                    <td> {{$lecture->lecture->course->name}} - {{$lecture->lecture->dayofweek}}
+                        <br> {{$lecture->lecture->starttime}} - {{$lecture->lecture->endtime}} </td>
+                    <td> {{$lecture->isActive}}</td>
+                    <td>{{$lecture->created_at}}</td>
+                    <td>{{$lecture->updated_at}}</td>
+                    <td>{!! $lecture->sendQRcode() !!}</td>
+                    <td>{!! $lecture->createHash() !!}</td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </table>
+    @endif
+</div>
+</body>
+</html>

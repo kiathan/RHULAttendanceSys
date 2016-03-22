fb514c3d8a098c1887194f8e5aa847432f9c2a08
bartal veyhe <bartal35@gmail.com>
2016-03-22 22:35:43 +0000

    Add in button to show own attends and also to show the list of all students attends

--- web-server/app/Http/routes.php
+++ web-server/app/Http/routes.php
@@ -41,8 +41,10 @@ Route::group(array('middleware' => 'auth'), function () {
 
     Route::get('/qr', 'qrCodeController@show');
 
-    Route::get('/overall', 'attendanceController@show');
+    Route::get('/overall', 'attendanceController@index');
     Route::get('/overall/lecture', 'attendanceController@showLecture');
+    Route::get('/overall/self', 'attendanceController@show');
+
 
     Route::get('/admin/attendance', 'attendanceController@index');
 

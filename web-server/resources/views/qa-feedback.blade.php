a97311b7d46946162ccba6f642777c8a98ac7725
bartal veyhe <bartal35@gmail.com>
2016-03-23 15:07:56 +0000

    add in check to see if there is any questions at all

--- web-server/resources/views/qa-feedback.blade.php
+++ web-server/resources/views/qa-feedback.blade.php
@@ -1,56 +1,63 @@
 @extends('Master.layout')
 
 @section('content')
-    <div class="container-fluid">
-        <?php $counter = 0;?>
-        @foreach($questions as $question)
-            <div class="row">
-                <div class="col-xs-12">
-                    <div id="chart_div_{{$counter}}"></div>
+    @if(!isset($questions))
+        <div>
+            No lecture right now
+        </div>
+    @else
+        <div class="container-fluid">
+            <?php $counter = 0;?>
+            @foreach($questions as $question)
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div id="chart_div_{{$counter}}"></div>
+                    </div>
                 </div>
-            </div>
-            <?php ++$counter;?>
-        @endforeach
-    </div>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script>
-        google.charts.load('current', {packages: ['corechart', 'bar']});
-        <?php $counter = 0;?>
-        @foreach($questions as $question)
-            google.charts.setOnLoadCallback(drawMultSeries_{{$counter}});
+                <?php ++$counter;?>
+            @endforeach
+        </div>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script>
+            google.charts.load('current', {packages: ['corechart', 'bar']});
+            <?php $counter = 0;?>
+            @foreach($questions as $question)
+                google.charts.setOnLoadCallback(drawMultSeries_{{$counter}});
 
-        function drawMultSeries_{{$counter}}() {
-            var data_{{$counter}} = google.visualization.arrayToDataTable([
-                ['Question', ''],
-                    @foreach($qaOrder as $awnsers)
-                    <?php $questionResults = $question->result->where('awnser', $awnsers)->first()?>
-                    @if($questionResults == null)
-                ['{{$awnsers}}', 0],
-                    @else
-                ['{{$questionResults->awnser}}', {{$questionResults->count}}],
-                @endif
-                @endforeach
-            ]);
+            function drawMultSeries_{{$counter}}() {
+                var data_{{$counter}} = google.visualization.arrayToDataTable([
+                    ['Question', ''],
+                        @foreach($qaOrder as $awnsers)
+                        <?php $questionResults = $question->result->where('awnser', $awnsers)->first()?>
+                        @if($questionResults == null)
+                    ['{{$awnsers}}', 0],
+                        @else
+                    ['{{$questionResults->awnser}}', {{$questionResults->count}}],
+                    @endif
+                    @endforeach
+                ]);
 
-            var options = {
-                title: '',
-                chartArea: {width: '50%'},
-                hAxis: {
-                    title: 'Number of student awners current Question',
-                    minValue: 0
-                },
-                vAxis: {
-                    title: 'Question_{{$counter}}'
-                }
-            };
+                var options = {
+                    title: '',
+                    chartArea: {width: '50%'},
+                    hAxis: {
+                        title: 'Number of student awners current Question',
+                        minValue: 0
+                    },
+                    vAxis: {
+                        title: 'Question_{{$counter}}'
+                    }
+                };
 
-            var chart = new google.visualization.BarChart(document.getElementById('chart_div_{{$counter}}'));
-            chart.draw(data_{{$counter}}, options);
+                var chart = new google.visualization.BarChart(document.getElementById('chart_div_{{$counter}}'));
+                chart.draw(data_{{$counter}}, options);
 
-            <?php ++$counter;?>
-        }
-        @endforeach
+                <?php ++$counter;?>
+            }
+            @endforeach
 
-    </script>
+            @endif
+
+        </script>
 
 @stop
